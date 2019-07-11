8c97960f974d2e3daa50f5e72d894fe32b8e4e82
Lee, Byungman <pavane90@gmail.com>
2019-07-11 23:34:22 +0900

    std::endl

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "hi" << std::endl << "my name is "	<< "lee" << std::endl;
+	return 0;
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
